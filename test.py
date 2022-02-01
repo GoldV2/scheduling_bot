7e08a71bdab52300540475dd20a320697e9ab2ef
Rafael Almeida <rafaelpbcp@gmail.com>
2022-02-01 09:05:42 -0500

    Testing emails

--- test.py
+++ test.py
@@ -1,73 +1,22 @@
-import typing
+# Import smtplib for the actual sending function
+import smtplib
 
-import discord
-from discord.ext import commands
+# Import the email modules we'll need
+from email.message import EmailMessage
 
-import os
-import sys
-import traceback
+# Open the plain text file whose name is in textfile for reading.
+msg = EmailMessage()
+msg.set_content("Test email coming from the Scheduling Bot")
 
-from discord import Intents
-from discord.ext import commands
-from discord import Embed
-from dotenv import load_dotenv
+# me == the sender's email address
+# you == the recipient's email address
+msg['Subject'] = f'Scheduling Bot Test Email'
+msg['From'] = 'rafaelpbcp@gmail.com'
+msg['To'] = 'office@aigolearning.org'
 
-# Defines a custom Select containing colour options
-# that the user can choose. The callback function
-# of this class is called when the user changes their choice
-class Dropdown(discord.ui.Select):
-    def __init__(self):
-
-        # Set the options that will be presented inside the dropdown
-        options = [
-            discord.SelectOption(label='Red', description='Your favourite colour is red', emoji='🟥'),
-            discord.SelectOption(label='Green', description='Your favourite colour is green', emoji='🟩'),
-            discord.SelectOption(label='Blue', description='Your favourite colour is blue', emoji='🟦')
-        ]
-
-        # The placeholder is what will be shown when no option is chosen
-        # The min and max values indicate we can only pick one of the three options
-        # The options parameter defines the dropdown options. We defined this above
-        super().__init__(placeholder='Choose your favourite colour...', min_values=1, max_values=3, options=options)
-
-    async def callback(self, interaction: discord.Interaction):
-        # Use the interaction object to send a response message containing
-        # the user's favourite colour or choice. The self object refers to the
-        # Select object, and the values attribute gets a list of the user's 
-        # selected options. We only want the first one.
-        await interaction.response.send_message(f'Your favourite colour is {self.values}')
-
-
-class DropdownView(discord.ui.View):
-    def __init__(self):
-        super().__init__()
-
-        # Adds the dropdown to our view object.
-        self.add_item(Dropdown())
-
-
-class Bot(commands.Bot):
-    def __init__(self):
-        super().__init__(command_prefix=commands.when_mentioned_or('$'))
-
-    async def on_ready(self):
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        print('------')
-    
-    
-bot = Bot()
-
-
-@bot.command()
-async def colour(ctx):
-    """Sends a message with our dropdown containing colours"""
-
-    # Create the view containing our dropdown
-    view = DropdownView()
-
-    # Sending a message containing our view
-    await ctx.send('Pick your favourite colour:', view=view)
-
-
-load_dotenv("token.env")
-bot.run(os.getenv('DISCORD_TOKEN'))
+# Send the message via our own SMTP server.
+s = smtplib.SMTP(host='smtp.gmail.com', port=587)
+s.starttls()
+s.login('rafaelpbcp@gmail.com', 'rvljmowvcjphtzez')
+s.send_message(msg)
+s.quit()
